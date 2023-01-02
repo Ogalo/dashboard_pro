c61df882ff183b861acfe6294f79b0a8184fa448
Harrison Otieno <ogaloharrison@gmail.com>
2023-01-02 03:55:04 +0300

    Uncommentted Team, contacts, invoice form.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Routes, Route } from "react-router-dom";
 import TopBar from "./scenes/global/TopBar";
 import Dashboard from './scenes/dashboard';
 import SideBar from "./scenes/global/SideBar";
-// import Team from './scenes/team';
-// import Invoices from './scenes/invoices';
-// import Contacts from './scenes/contacts';
+import Team from './scenes/team';
+import Invoices from './scenes/invoices';
+import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
-// import Form from './scenes/form';
+import Form from './scenes/form';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import FAQ from './scenes/faq';
@@ -30,10 +30,10 @@ function App() {
           <TopBar />
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            {/* <Route path="/team" element={<Team />} /> */}
-            {/* <Route path="/contacts" element={<Contacts />} /> */}
-            {/* <Route path="/invoices" element={<Invoices />} /> */}
-            {/* <Route path="/form" element={<Form />} /> */}
+            <Route path="/team" element={<Team />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/invoices" element={<Invoices />} />
+            <Route path="/form" element={<Form />} />
             {/* <Route path="/bar" element={<Bar />} /> */}
             {/* <Route path="/pie" element={<Pie />} /> */}
             {/* <Route path="/line" element={<Line />} /> */}
